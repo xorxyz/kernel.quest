24b1105307ef407b2e5f0a627cc8954a8717dd14
Jonathan Dupré <hello@jonathandupre.com>
2021-03-25 11:14:17 -0400

    assets & js

--- build.sh
+++ build.sh
@@ -2,10 +2,17 @@
 
 mkdir -p public
 cp style.css public/style.css
+cp script.js public/script.js
 
-files="index start blog"
+assets="favicon.ico"
 
-for f in $files; do
-  head=$(sed "s/{{ PAGE_TITLE }}/$f/" head.html)
-  echo "$head" | cat - nav.html "$f.html" foot.html > "public/$f.html"
+for a in $assets; do
+  cp "assets/$a" "public/$a"
+done
+
+pages="index start blog"
+
+for p in $pages; do
+  head=$(sed "s/{{ PAGE_TITLE }}/$p/" head.html)
+  echo "$head" | cat - nav.html "$p.html" foot.html > "public/$p.html"
 done
