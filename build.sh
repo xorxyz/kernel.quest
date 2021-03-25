64fb44911bd44036166a4cbbb80947772817dcfe
Jonathan Dupré <hello@jonathandupre.com>
2021-03-25 00:08:44 -0400

    start page

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-files="index test"
-
 mkdir -p public
 cp style.css public/style.css
 
+# index is a special case
+cat head.html "index.html" foot.html > "public/index.html"
+
+files="start"
+
 for f in $files; do 
-  cat head.html "$f.html" foot.html > "public/$f.html"
+  cat head.html nav.html "$f.html" foot.html > "public/$f.html"
 done
