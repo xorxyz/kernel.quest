585aeb15e5bd0605a743f2392c8fd42db0ce1578
Jonathan Dupré <hello@jonathandupre.com>
2021-03-25 16:43:04 -0400

    minor fixes

--- build.sh
+++ build.sh
@@ -9,9 +9,14 @@ for src in assets/*; do
   cp "$src" "$dest"
 done
 
-pages="index start blog"
+pages="index game blog logs"
+today=$(date "+%Y-%m-%d%n")
 
 for p in $pages; do
   head=$(sed "s/{{ PAGE_TITLE }}/$p/" head.html)
-  echo "$head" | cat - nav.html "$p.html" foot.html > "public/$p.html"
+  content=$(echo "$head" | cat - nav.html "$p.html" foot.html)
+
+  echo "$content" \
+    | sed "s/{{ LAST_UPDATE }}/$(printf '%-17s' "$today")/" \
+    > "public/$p.html"
 done
