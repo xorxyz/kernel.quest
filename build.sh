4c86ea39ed83e12b625e9986f47e796c28ba1a61
Jonathan Dupré <hello@jonathandupre.com>
2021-03-26 17:22:45 -0400

    logs page\?

--- build.sh
+++ build.sh
@@ -5,11 +5,20 @@ today=$(date "+%Y-%m-%d%n")
 cp -r assets public
 cp ./*.css ./*.js public
 
+nav=$(cat nav.html)
+foot=$(cat foot.html)
+txt=$(git count-objects)
+
 head=$(sed "s/{{ PAGE_TITLE }}//" head.html)
 content=$(echo "$head" | cat - nav.html "index.html" foot.html)
 echo "$content" > "public/index.html"
 
-pages="game blog wiki logs gp sheep herbs"
+head=$(sed "s/{{ PAGE_TITLE }}/logs/" head.html)
+logs=$(sed "s/{{ LOGS }}/$txt/" logs.html )
+content=$(printf "%s\n%s\n%s\n%s\n" "$head" "$nav" "$logs" "$foot")
+echo "$content" > "public/logs.html"
+
+pages="game blog wiki gp sheep herbs"
 
 for p in $pages; do
   head=$(sed "s/{{ PAGE_TITLE }}/$p/" head.html)
