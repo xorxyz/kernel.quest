6f28770c070a46f06dbe38c6ca2f4ddda0e7fa90
Jonathan Dupré <hello@jonathandupre.com>
2021-03-26 19:20:50 -0400

    animations?

---
+++ index.js
@@ -0,0 +1,45 @@
+const swap = ([a, b]) => [b, a];
+const rotate3 = ([a, b, c]) => [b, c, a];
+const rotate5 = ([a, b, c, d, e]) => [b, c, d, e, a];
+
+const LOOP_SECONDS = 0.1;
+const LOOP_MS = LOOP_SECONDS * 1000;
+
+let snail = ['🐌__', '__🐌'];
+let sheep = ['🌿🐑', '🐑🌿'];
+let grass = ['`', `'`];
+let seconds = 0;
+let timer = 0;
+
+const room = document.getElementById('room');
+const player = document.getElementById('player');
+
+const draw = (pair) => {
+  room.innerText = room.innerText.replace(new RegExp(pair[0], 'g'), pair[1]);
+  return pair;
+}
+
+const cycle = () => {
+  seconds += LOOP_SECONDS;
+
+  if (seconds % 0.4 === 0) {
+    grass = swap(grass);
+    draw(grass);
+  }
+
+  if (seconds % 0.8 === 0) {
+    sheep = swap(sheep);
+    draw(sheep);
+
+    snail = swap(snail);
+    draw(snail);
+  }
+}
+
+player.addEventListener('play', () => {
+  timer = setInterval(cycle, LOOP_MS);
+});
+
+player.addEventListener('pause', () => {
+  clearInterval(timer);
+});
