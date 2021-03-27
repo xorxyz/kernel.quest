a8fa2ec6a309ce84bbd7002423063f6bbb26caba
Jonathan Dupré <hello@jonathandupre.com>
2021-03-27 15:17:30 -0400

    refactor

--- build.sh
+++ build.sh
@@ -1,30 +1,29 @@
 #!/bin/sh
 
+mkdir -p public       && \
+cp -r assets/ public/ && \
+cp -r js/*    public/
+
+head="components/head.html"
+nav="components/nav.html"
+foot="components/foot.html"
+
+print_page() (
+  sed "s/{{ PAGE_TITLE }}/$1/" "$head" | cat - "$nav" "$2" "$foot"
+)
+
+set_var() ( 
+  next=$(printf "%-${#1}s" "$2")
+  echo "s/$1/$next/"
+)
+
+pages="game blog wiki edit logs gp sheep herbs"
 today=$(date "+%Y-%m-%d%n")
 
-cp -r assets public
-cp ./*.css ./*.js public
-
-nav=$(cat nav.html)
-foot=$(cat foot.html)
-txt=$(git count-objects)
-
-head=$(sed "s/{{ PAGE_TITLE }}//" head.html)
-content=$(echo "$head" | cat - nav.html "index.html" foot.html)
-echo "$content" > "public/index.html"
-
-head=$(sed "s/{{ PAGE_TITLE }}/logs/" head.html)
-logs=$(sed "s/{{ LOGS }}/$txt/" logs.html )
-content=$(printf "%s\n%s\n%s\n%s\n" "$head" "$nav" "$logs" "$foot")
-echo "$content" > "public/logs.html"
-
-pages="game blog wiki gp sheep herbs"
+print_page "" "pages/index.html" > "public/index.html"
 
 for p in $pages; do
-  head=$(sed "s/{{ PAGE_TITLE }}/$p/" head.html)
-  content=$(echo "$head" | cat - nav.html "$p.html" foot.html)
-
-  echo "$content" \
-    | sed "s/{{ LAST_UPDATE }}/$(printf '%-17s' "$today")/" \
+  print_page "$p" "pages/$p.html" \
+    | sed "$(set_var 'LAST_UPDATE' "$today")" \
     > "public/$p.html"
 done
