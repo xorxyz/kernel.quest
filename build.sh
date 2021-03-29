f3d99eed409d14d4ba8317b85cabb2b9a5ec27ef
Jonathan Dupré <hello@jonathandupre.com>
2021-03-29 12:42:58 -0400

    simplify build script

--- build.sh
+++ build.sh
@@ -1,28 +1,20 @@
 #!/bin/sh
 
+today=$(date "+%Y-%m-%d%n")
+pages=$(find pages/* | sed 's/pages\///g;s/.html//' | grep -v index)
+
+print_page() (
+  dir="components"
+  cat "$dir/head.html" "$dir/nav.html" "$2" "$dir/foot.html" \
+  | sed -e "s/{{ PAGE_TITLE }}/$1/"\
+        -e "s/{{ LAST_UPDATE }}/$today/"
+)
+
 cp -r assets/ public/ && \
 cp -r js/*    public/
 
-head="components/head.html"
-nav="components/nav.html"
-foot="components/foot.html"
-
-print_page() (
-  sed "s/{{ PAGE_TITLE }}/$1/" "$head" | cat - "$nav" "$2" "$foot"
-)
-
-set_var() ( 
-  next=$(printf "%-${#1}s" "$2")
-  echo "s/$1/$next/"
-)
-
-pages="game blog wiki edit logs gp sheep herbs"
-today=$(date "+%Y-%m-%d%n")
-
-print_page " " "pages/index.html" > "public/index.html"
-
 for p in $pages; do
-  print_page "$p" "pages/$p.html" \
-    | sed "$(set_var 'LAST_UPDATE' "$today")" \
-    > "public/$p.html"
+  print_page "| $p | " "pages/$p.html" > "public/$p.html"
 done
+
+print_page "|" "pages/index.html" > "public/index.html"
