d729089adf6b6abe6102be6a19674e09673c323a
Jonathan Dupré <hello@jonathandupre.com>
2021-04-03 23:10:26 -0400

    imported v0.1 of interpreter

--- js/game.js
+++ js/game.js
@@ -1,11 +1,24 @@
+import Interpreter from './interpreter.js';
+
 document.addEventListener('DOMContentLoaded', () => {
+  console.log('game.js: DOMContentLoaded');
   const el = document.getElementById('prompt');
+  const sh = new Interpreter();
 
   if (!el) return;
 
   el.addEventListener('change', e => {
     e.preventDefault();
     
-    document.location.search += '&' + el.value;
+    console.log(e.target.value)
+
+    try {
+      const result = sh.exec(e.target.value);
+      console.log(result)
+    } catch (err) {
+      console.error(err)
+    }
+
+    e.target.value = ''
   });
 })
